9be9a3621e7461f29a236443bd3d62838b03f13f
vanshikajain18 <64839044+vanshikajain18@users.noreply.github.com>
2020-06-06 03:06:19 +0530

    Create day2.cpp

---
+++ day2.cpp
@@ -0,0 +1,43 @@
+TASK 2 
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t ;
+    cin>>t ;
+    while(t--)
+    {
+        int n ;
+        cin>>n ;
+        int k ;
+        cin>>k ;
+        int a[n] ;
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i] ;
+        }
+        
+        int l=0 , h=n-1 , count=-1 ;
+        while(l<=h)
+        {
+          int mid=(l+h)/2 ;
+          
+          if(l==k || h==k || a[mid]==k)
+          {count=1 ;
+          break ; }
+          
+          else if(a[mid]<k)
+          l=mid+1 ;
+          
+          else if(a[mid]>k)
+          h=mid-1 ;
+          
+        }
+        
+        cout<<count<<"\n" ;
+    }
+	
+	return 0;
+}
