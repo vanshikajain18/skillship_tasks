2a4df3f9daea850ec430a61f57b350ef8dbb2e5d
vanshikajain18 <64839044+vanshikajain18@users.noreply.github.com>
2020-06-06 13:54:19 +0530

    Update day3.cpp

--- day3.cpp
+++ day3.cpp
@@ -26,3 +26,43 @@ while(t--)
 }
 	return 0;
 }
+
+
+
+TASK 2 
+	
+	#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n ;
+	    cin>>n ;
+	    int a[10]={0} ;
+	    
+	    for(int i=0;i<n;i++)
+	    {   int num ;
+	        cin>>num ;
+	        while(num>0)
+	        {
+	        a[num%10]++ ;
+	        num=num/10 ;
+	        }
+	    }
+	    
+	    for(int i=0;i<10;i++)
+	    {
+	        if(a[i] !=0)
+	        cout<<i<<" " ;
+	    }
+	    
+	   cout<<"\n" ;
+	}
+	
+
+return 0;
+}
