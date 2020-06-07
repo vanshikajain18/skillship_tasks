8a68c1c1f35276e4fc8ac14fa153b507bd28c048
vanshikajain18 <64839044+vanshikajain18@users.noreply.github.com>
2020-06-07 20:12:39 +0530

    Update day1_task2,3.cpp

--- day1_task2,3.cpp
+++ day1_task2,3.cpp
@@ -1,3 +1,33 @@
+TASK 1 
+
+	class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target)
+    {   int n= numbers.size();
+        vector<int> v ;
+        
+        for(int i=0 ;i<n;i++)
+        {   if(i>0 && numbers[i]==numbers[i-1] )
+            continue ;
+         
+            for(int j=i+1 ;j<n; j++)
+            {  
+                 if(j>0 && numbers[j]==numbers[j-1] && numbers[j]+numbers[j-1]!=target)
+            continue ;
+         
+                if (numbers[i] +numbers[j] == target )
+                { v.push_back(i+1) ;
+                    v.push_back(j+1) ;
+                    return v;}
+            }
+        
+        }
+        
+    return v ;}
+
+};
+
+
 TASK 2 
 
 #include <iostream>
