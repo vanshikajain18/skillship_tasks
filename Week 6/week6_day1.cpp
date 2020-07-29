cc0c78bc09439adf5d4c87f985dc44b290afe1ce
vanshikajain18 <64839044+vanshikajain18@users.noreply.github.com>
2020-07-29 16:15:36 +0530

    Update week6_day1.cpp

--- Week 6/week6_day1.cpp
+++ Week 6/week6_day1.cpp
@@ -21,3 +21,56 @@ int main() {
 
 
 }
+
+
+TASK 2
+K SPECIAL CELLS
+
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+int main() {
+    
+    int t;
+    cin>>t ;
+    while(t--)
+    {   
+    int n,m,k ;
+    cin>>n>>m>>k ;
+    
+    int **a ;
+    a=new int*[n] ;
+    for(int i=0 ;i<n;i++)
+    {
+        a[i]=new int[m] ;
+    }
+ 
+        for(int i=0 ;i<n ;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+              if(i==0 || j==0)
+              a[i][j]=1 ;
+ 
+              else a[i][j]= a[i-1][j]+a[i][j-1] ;
+            }
+ 
+        }
+        
+        long long int sum=0 ;
+    
+        for(int i=0 ;i<k ;i++)
+        {
+           int x,y,p ;
+           cin>>x>>y>>p ;
+           a[x-1][y-1]=a[x-1][y-1]*p ;
+           sum=(sum+a[x-1][y-1]) %1000000007 ;
+        }
+        
+        cout<<sum<<"\n";
+ 
+    }	
+ 
+}
